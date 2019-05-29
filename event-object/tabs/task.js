9ac5b070f76363e452a8fcb281249eb64e17d071
Ivan Kravtsov <cold67@mail.ru>
2019-05-29 17:20:20 +0300

    tabs with delegation

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,24 +1,24 @@
-const tabs = [...document.querySelectorAll('.tab')],
-        tabsArrLength = tabs.length;
+const tabNav = document.querySelector('.tab__navigation'),
+        tabs = [...tabNav.querySelectorAll('.tab')],
+            tabsNumber = tabs.length;
 
 const tabsContent = [...document.querySelectorAll('.tab__content')];
 
-for ( let i = 0; i < tabsArrLength; i++ ) {
-    tabs[i].addEventListener('click', () => {
-        switchTab(tabs[i]);
-    });
-};
 
-function switchTab (tab) {
-    if(!tab.classList.contains('tab_active')) {
-        removeActiveTabs();
-        tab.classList.add('tab_active');
-        tabsContent[tabs.indexOf(tab)].classList.add('tab__content_active');
+tabNav.addEventListener('click', switchTab);
+
+function switchTab (e) {
+    if(!e.target.classList.contains('tab_active') && e.target.classList.contains('tab')) {
+        
+        removeActiveTabs ();
+        
+        e.target.classList.add('tab_active');
+        tabsContent[tabs.indexOf(e.target)].classList.add('tab__content_active');
     };
 };
 
 function removeActiveTabs () {
-    for ( let tab = 0; tab < tabsArrLength; tab++) {
+    for ( let tab = 0; tab < tabsNumber; tab++) {
         tabs[tab].classList.remove('tab_active');
         tabsContent[tab].classList.remove('tab__content_active');
     };
